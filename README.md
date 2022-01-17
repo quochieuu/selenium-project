6264ae465e2a0d8af4adc5e260685bd2d620686d
Hieu <hieuho@geckoso.com>
2022-01-17 11:54:17 +0700

    implement case test_register_fail_with_used_phone_number

--- README.md
+++ README.md
@@ -3,9 +3,5 @@
 ## Registration
 ``` 
 bash
-py registration-test.py test_register_success_with_valid_account
-
-py registration-test.py test_register_fail_with_invalid_phone_number
-
-py registration-test.py test_register_fail_with_invalid_password
+py registration-test.py
 ```
--- registration-test.py
+++ registration-test.py
@@ -66,6 +66,31 @@ def test_register_fail_with_invalid_password():
     driver.quit()
 
 
+def test_register_fail_with_used_phone_number():
+    url = "https://phongtro123.com/dang-ky-tai-khoan"
+    driver.get(url)
+    get_url = driver.current_url
+    
+    driver.find_element_by_id("inputFullName").send_keys("Hieu Ho")   
+    driver.find_element_by_id("inputPhone").send_keys("0983058005") 
+    driver.find_element_by_id("password").send_keys("Abc123!@#")    
+    driver.find_element_by_class_name("btn-submit").submit()   
+
+    register_success_url = "https://phongtro123.com/xac-thuc-tai-khoan"
+    print(url)
+    print(get_url)
+    if get_url == url:
+        print("PASS")
+    elif get_url == register_success_url:
+        print("FAIL")
+    else:
+        print("FAIL")
+
+    driver.quit()
+
+
+
 # test_register_success_with_valid_account()
 # test_register_fail_with_invalid_phone_number()
 # test_register_fail_with_invalid_password()
+test_register_fail_with_used_phone_number()
